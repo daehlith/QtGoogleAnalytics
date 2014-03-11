8cd203f0e915c11826b6486bad83d932880bce1d
Thomas Daehling <thomas@jivaro.com>
2014-03-11 17:57:35 +0000

    Tons of stuff: - small changes to the cmake project so we get rid off the CMP 0020 policy warning - restructured build artefacts to end up in the same folder so that I can run tests as post-build step from Qt Creator - added a basic configuration class - started adding support for all Google Analytics parameters, even if they might not make sense right now - some more tests

--- src/QtGA.cpp
+++ src/QtGA.cpp
@@ -19,15 +19,24 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 #include <QtCore/QRegExp>
+#include <QtCore/QUrlQuery>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkRequest>
 
 #include <QtGA/QtGA.h>
 
-QtGA::QtGA(QObject *parent) : QObject(parent)
+const QUrl QtGAConfiguration::SECURE_ENDPOINT = QUrl("https://ssl.google-analytics.com/collect");
+const QUrl QtGAConfiguration::NORMAL_ENDPOINT = QUrl("https//www.google-analytics.com/collect");
+const QString QtGAConfiguration::USERAGENT = QString("QtGA/1.0");
+
+QtGAConfiguration::QtGAConfiguration()
+    : m_trackingID(""), m_endpoint(QtGAConfiguration::NORMAL_ENDPOINT), m_requestType(QtGAConfiguration::DefaultRequest),
+      m_userAgent(QtGAConfiguration::USERAGENT), m_cacheBusting(false)
 {
 
 }
 
-void QtGA::setTrackingID(const QString& trackingID)
+void QtGAConfiguration::setTrackingID(const QString& trackingID)
 {
     QRegExp validTrackingID("\\bUA-\\d{4,10}-\\d{1,4}\\b");
     if ( validTrackingID.exactMatch(trackingID) )
@@ -40,7 +49,124 @@ void QtGA::setTrackingID(const QString& trackingID)
     }
 }
 
-const QString& QtGA::trackingID() const
+const QString& QtGAConfiguration::trackingID() const
 {
     return m_trackingID;
+}
+
+bool QtGAConfiguration::isSecure() const
+{
+#ifndef QT_NO_SSL
+    return m_endpoint.scheme() == "https";
+#else
+    return false;
+#endif
+}
+
+void QtGAConfiguration::setEndpoint(const QUrl &endpoint)
+{
+#ifdef QT_NO_SSL
+    if (endpoint.scheme() == "https")
+    {
+        m_endpoint = "";
+    }
+    else
+    {
+        m_endpoint = endpoint;
+    }
+#else
+    m_endpoint = endpoint;
+#endif
+}
+
+const QUrl& QtGAConfiguration::endpoint() const
+{
+    return m_endpoint;
+}
+
+void QtGAConfiguration::setIPAnonymization(bool anonymous)
+{
+    m_anonymizeIP = anonymous;
+}
+
+bool QtGAConfiguration::anonymizeIP() const
+{
+    return m_anonymizeIP;
+}
+
+void QtGAConfiguration::setUserAgent(const QString& userAgent)
+{
+    m_userAgent = userAgent;
+}
+
+QString QtGAConfiguration::userAgent() const
+{
+    return m_userAgent;
+}
+
+void QtGAConfiguration::setClientID(const QUuid &clientID)
+{
+    if ( ! m_clientID.isNull() && clientID.version() == QUuid::Random )
+    {
+        m_clientID = clientID;
+    }
+}
+
+QUuid QtGAConfiguration::clientID() const
+{
+    return m_clientID;
+}
+
+QtGA::QtGA(QObject *parent) : QtGA( QtGAConfiguration(), parent)
+{
+
+}
+
+QtGA::QtGA(const QtGAConfiguration &config, QObject *parent) : QObject(parent), m_config(config), m_nam(nullptr), m_session(NoSession)
+{
+    QNetworkAccessManager* obj = qobject_cast<QNetworkAccessManager*>(parent);
+    if (obj)
+    {
+        m_nam = obj;
+    }
+}
+
+void QtGA::setNetworkAccessManager(QNetworkAccessManager *manager)
+{
+    if ( manager )
+    {
+        setParent(manager);
+        m_nam = manager;
+    }
+}
+
+const QNetworkAccessManager* QtGA::networkAccessManager() const
+{
+    return m_nam;
+}
+
+void QtGA::startSession()
+{
+    m_session = StartSession;
+}
+
+void QtGA::endSession()
+{
+    m_session = EndSession;
+}
+
+QtGA::AnalyticsError QtGA::track(HitTypes type, const QVariantMap &parameters, bool interactive)
+{
+    if ( m_nam == nullptr )
+    {
+        return ErrorNoNetworkManager;
+    }
+
+    QNetworkRequest req(m_config.endpoint());
+    req.setHeader(QNetworkRequest::UserAgentHeader, m_config.userAgent());
+    QUrlQuery query;
+    QByteArray data = query.toString(QUrl::FullyEncoded).toLatin1();
+    m_nam->post( req, data );
+
+    return NoError;
 }
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,20 +1,59 @@
 #include <QString>
 
 #include <gtest/gtest.h>
+#include <gtest/gtest-spi.h>
 
 #include <QtGA/QtGA.h>
 
-TEST(QtGA_Tests, tracking_id_test)
+TEST(QtGAConfiguration_Tests, tracking_id_test)
 {
     const QString longTrackingID("UA-9876543210-1234");
     const QString shortTrackingID("UA-4321-0");
     const QString invalidTrackingID("FOO-BAR-BAZ");
 
-    QtGA ga;
-    ga.setTrackingID(longTrackingID);
-    EXPECT_EQ(longTrackingID, ga.trackingID());
-    ga.setTrackingID(shortTrackingID);
-    EXPECT_EQ(shortTrackingID, ga.trackingID());
-    ga.setTrackingID(invalidTrackingID);
-    EXPECT_EQ(QString(""), ga.trackingID());
+    QtGAConfiguration configuration;
+    configuration.setTrackingID(longTrackingID);
+    EXPECT_EQ(longTrackingID, configuration.trackingID());
+    configuration.setTrackingID(shortTrackingID);
+    EXPECT_EQ(shortTrackingID, configuration.trackingID());
+    configuration.setTrackingID(invalidTrackingID);
+    EXPECT_EQ(QString(""), configuration.trackingID());
+}
+
+TEST(QtGAConfiguration_Tests, connector_test)
+{
+    QtGAConfiguration configuration;
+    EXPECT_FALSE(configuration.isSecure());
+    configuration.setEndpoint(QtGAConfiguration::SECURE_ENDPOINT);
+#ifndef QT_NO_SSL
+    EXPECT_TRUE(configuration.isSecure());
+#else
+    EXPECT_NONFATAL_FAILURE(EXPECT_TRUE(configuration.isSecure()), "Qt was built without SSL support");
+#endif
+}
+
+TEST(QtGAConfiguration_Tests, endpoint_tests)
+{
+    QtGAConfiguration configuration;
+    EXPECT_EQ(QtGAConfiguration::NORMAL_ENDPOINT, configuration.endpoint());
+    configuration.setEndpoint(QtGAConfiguration::SECURE_ENDPOINT);
+#ifndef QT_NO_SSL
+    EXPECT_EQ(QtGAConfiguration::SECURE_ENDPOINT, configuration.endpoint());
+#else
+    EXPECT_NONFATAL_FAILURE(EXPECT_EQ(QtGAConfiguration::SECURE_ENDPOINT, configuration.endpoint()), "Qt was built without SSL support");
+#endif
+}
+
+TEST(QtGAConfiguration_Tests, client_id_tests)
+{
+    // The value of this field should be a random UUID (version 4) as described in http://www.ietf.org/rfc/rfc4122.txt
+    QtGAConfiguration configuration;
+    EXPECT_TRUE(configuration.clientID().isNull());
+//    const QUuid expected = QUuid("f47ac10b-58cc-4372-a567-0e02b2c3d479");
+//    configuration.setClientID(expected);
+//    EXPECT_EQ(expected, configuration.clientID());
+//    // An invalid clientID should not change the existing value
+//    const QUuid noVersion4Uuid = QUuid("f47ac10b-58cc-5372-b567-0e02b2c3d479");
+//    configuration.setClientID(noVersion4Uuid);
+//    EXPECT_EQ(expected, configuration.clientID());
 }
